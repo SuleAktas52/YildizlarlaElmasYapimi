4cf2edcf6f232dced1e4ffed6f08bb984d5bb7ea
Şule Aktaş <suleaktasgs@gmail.com>
2022-08-21 15:02:45 +0300

    Create YildizlarlaElmasYapimi.java

---
+++ YildizlarlaElmasYapimi.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class YildizlarlaElmasYapimi {
+    public static void main(String[] args) {
+
+        Scanner klavye = new Scanner(System.in);
+        System.out.print("Bir Sayi Giriniz :");
+        int n = klavye.nextInt();
+
+        for (int i = 0; i <= n ; i++) {
+            for (int j = 0; j < (n - i); j++) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= (2 * i + 1); k++) {
+                System.out.print("*");
+            }
+            System.out.println(" ");
+        }
+        for (int i = n; i >= 1; i--) {
+
+            for (int j =0; j <= (n-i); j++) {
+                System.out.print(" ");
+            }
+            for (int k = (2 * i - 1); k >= 1; k--) {
+                System.out.print("*");
+            }
+            System.out.println(" ");
+        }
+
+    }
+}
+
+/*
+     *
+    ***
+   *****
+  *******
+ *********
+  *******
+   *****
+    ***
+     *
+ */
